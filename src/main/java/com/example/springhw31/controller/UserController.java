76c127cd52c474df3038122443f90bba9d786f29
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-10-07 17:27:10 +0900

    init

--- src/main/java/com/example/springhw31/controller/UserController.java
+++ src/main/java/com/example/springhw31/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.example.springhw31.controller;
 
+import com.example.springhw31.dto.UserDto;
+
 public class UserController {
 
+
+  public UserDto join(UserDto userDto) {
+
+  }
 }
--- src/main/java/com/example/springhw31/dto/UserDto.java
+++ src/main/java/com/example/springhw31/dto/UserDto.java
@@ -1,5 +1,19 @@
 package com.example.springhw31.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * UserDto 클래스는 수정 X
+ */
+@Getter
+@Setter
 public class UserDto {
+
+  private String username;
+
+  private String password;
+
+  private String nickname;
 
 }
