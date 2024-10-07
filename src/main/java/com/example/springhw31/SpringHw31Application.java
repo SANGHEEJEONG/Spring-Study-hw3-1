f83066680e2252bc43c5113f08f5f9be5833597d
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-10-07 16:55:31 +0900

    init

---
+++ src/main/java/com/example/springhw31/SpringHw31Application.java
@@ -0,0 +1,13 @@
+package com.example.springhw31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHw31Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringHw31Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springhw31/SpringHw31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhw31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw31ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
