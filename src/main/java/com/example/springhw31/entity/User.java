fa1a12ef6117edf2e21c2d243d9caf2af50884a2
JiHoon Baek <133375485+Chuseok22@users.noreply.github.com>
2024-10-08 14:41:30 +0900

    init

--- src/main/java/com/example/springhw31/entity/User.java
+++ src/main/java/com/example/springhw31/entity/User.java
@@ -2,4 +2,9 @@ package com.example.springhw31.entity;
 
 public class User {
 
+  private String username;
+
+  private String password;
+
+  private String nickname;
 }
--- src/main/java/com/example/springhw31/service/UserService.java
+++ src/main/java/com/example/springhw31/service/UserService.java
@@ -2,6 +2,9 @@ package com.example.springhw31.service;
 
 import com.example.springhw31.dto.UserDto;
 
+/**
+ * Controller에 반환할 때도 DTO 객체를 반환합니다.
+ */
 public class UserService {
 
   public UserDto join(UserDto userDto) {
